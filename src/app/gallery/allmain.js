58e4d482a3ed80f9b0692c217f732e7efb888e8b
abuzar02 <mdabujaralam3@gmail.com>
2024-09-06 21:55:17 +0530

    done

--- src/app/gallery/allmain.js
+++ src/app/gallery/allmain.js
@@ -1,60 +1,61 @@
-"use client";
-import React, { useState } from 'react';
-import 'react-image-lightbox/style.css';
-import Lightbox from 'react-image-lightbox';
-import '../style/gallery.css'
-import { IoSearchSharp } from "react-icons/io5";
+  "use client";
+  import React, { useState } from 'react';
+  import 'react-image-lightbox/style.css';
+  import Lightbox from 'react-image-lightbox';
+  import '../style/gallery.css'
+  import { IoSearchSharp } from "react-icons/io5";
 
-const LightboxComponent = ({ images }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [photoIndex, setPhotoIndex] = useState(0);
+  const LightboxComponent = ({ images }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [photoIndex, setPhotoIndex] = useState(0);
 
-  const handleViewLargerClick = (index) => {
-    setPhotoIndex(index);
-    setIsOpen(true);
-  };
+    const handleViewLargerClick = (index) => {
+      setPhotoIndex(index);
+      setIsOpen(true);
+    };
 
-  const handleMovePrev = () => {
-    setPhotoIndex((photoIndex + images.length - 1) % images.length);
-  };
+    const handleMovePrev = () => {
+      setPhotoIndex((photoIndex + images.length - 1) % images.length);
+    };
 
-  const handleMoveNext = () => {
-    setPhotoIndex((photoIndex + 1) % images.length);
-  };
+    const handleMoveNext = () => {
+      setPhotoIndex((photoIndex + 1) % images.length);
+    };
 
-  return (
-    <>
-    <div className='photoGallery'><h2>Photo Gallery</h2></div>
-    <div className='thumnail'>
-      {images.map((src, index) => (
-        <div 
-          key={index} 
-          className='mainImage'
-          onClick={() => handleViewLargerClick(index)}
-        >
-          <img
-            src={src}
-            alt={`Image ${index}`}
-          />
-          <div className='overlay'>
-          <IoSearchSharp />
+    return (
+      <>
+      <div className='photoGallery'><h2>Photo Gallery</h2></div>
+      <div className='thumnail'>
+        {images.map((src, index) => (
+          <div 
+            key={index} 
+            className='mainImage'
+            onClick={() => handleViewLargerClick(index)}
+          >
+            <img
+              src={src}
+              alt={`Image ${index}`}
+            />
+            <div className='overlay'>
+            <IoSearchSharp />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
 
-      {isOpen && (
-        <Lightbox
-          mainSrc={images[photoIndex]}
-          nextSrc={images[(photoIndex + 1) % images.length]}
-          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-          onCloseRequest={() => setIsOpen(false)}
-          onMovePrevRequest={handleMovePrev}
-          onMoveNextRequest={handleMoveNext}
-        />
-      )}
-    </div>
-    </>
-  );
-};
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            onCloseRequest={() => setIsOpen(false)}
+            onMovePrevRequest={handleMovePrev}
+            onMoveNextRequest={handleMoveNext}
+          />
+        )}
+      </div>
+      </>
+    );
+  };
 
-export default LightboxComponent;
+  export default LightboxComponent;
+
